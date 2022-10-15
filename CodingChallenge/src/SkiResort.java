553e3ff5962bf7e227816d6b4d048fe44be2f6a2
juliu <juliu@DESKTOP-87728DV>
2022-10-15 20:23:29 +0800

    add code

--- CodingChallenge/src/SkiResort.java
+++ CodingChallenge/src/SkiResort.java
@@ -1,15 +1,27 @@
 
 public class SkiResort {
-
+	private static final int[][] mountainMap = {{4,8,7,3}, {2,5,9,3}, {6,3,2,5}, {4,4,1,6}};
+	private PathDetails[][] wholeMapDetails; 
+	
 	public static void main(String[] args)
     {
-   
+		SkiResort resort = new SkiResort();
     }
 	
 	public SkiResort() {
-		int[][] mountainMap = {{4,8,7,3}, {2,5,9,3}, {6,3,2,5}, {4,4,1,6}};
+		wholeMapDetails = new PathDetails[mountainMap.length][];
+		for(int x = 0; x < mountainMap.length; x++) {
+			wholeMapDetails[x] = new PathDetails[mountainMap[x].length];
+		}
+	}
+	
+	/*
+	private int[] getLongestPath() {
+		int[] longestPath;
+		
 		
 	}
+	*/
 }
 
 
