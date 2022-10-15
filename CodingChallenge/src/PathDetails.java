0586a64db32563e2d42a201d144d584a60526e5c
juliu <juliu@DESKTOP-87728DV>
2022-10-15 20:02:46 +0800

    add class

---
+++ CodingChallenge/src/PathDetails.java
@@ -0,0 +1,13 @@
+
+public class PathDetails {
+
+	int startHeight, endHeight, length, xNext, yNext;
+	
+	public PathDetails(int startHeight, int endHeight, int length, int xNext, int yNext) {
+		this.startHeight = startHeight;
+		this.endHeight = endHeight;
+		this.length = length;
+		this.xNext = xNext;
+		this.yNext = yNext;
+	}
+}
